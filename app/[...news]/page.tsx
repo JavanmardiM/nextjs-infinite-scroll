058127ba92a17f5e8cb50e139498920795f5eb5e
javanmardi <javanmardi.ej@gmail.com>
2025-02-06 17:14:01 +0330

    feat: add auto fetch

--- app/[...news]/page.tsx
+++ app/[...news]/page.tsx
@@ -1,16 +1,21 @@
 "use client";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInView } from "react-intersection-observer";
 import { getNews } from "./lib";
 import Post from "./post";
 import { useParams, useRouter } from "next/navigation";
 import { title } from "@/components/primitives";
 import { News } from "@/types";
+import { useEffect, useCallback } from "react";
 
 export default function Page() {
   const router = useRouter();
   const params = useParams();
+  const { ref, inView } = useInView();
 
-  const currentPage = Array.isArray(params.news) ? parseInt(params.news[2]) : 1;
+  const currentPage = Array.isArray(params.news)
+    ? parseInt(params.news[2]) || 1
+    : 1;
 
   const {
     data,
@@ -19,35 +24,47 @@ export default function Page() {
     fetchNextPage,
     hasNextPage,
   } = useInfiniteQuery({
-    queryKey: ["news", currentPage],
+    queryKey: ["news"],
     queryFn: getNews,
     initialPageParam: currentPage,
     getNextPageParam: (lastPage) => lastPage.nextPage,
   });
 
-  const handleNextPage = () => {
+  const handleNextPage = useCallback(() => {
     if (hasNextPage) {
       fetchNextPage();
       const nextPage = currentPage + 1;
 
-      router.push(`/news/page/${nextPage}`);
+      router.push(`/news/page/${nextPage}`, { scroll: false });
     }
-  };
+  }, [hasNextPage, fetchNextPage, currentPage, router]);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+
+    if (inView && hasNextPage) {
+      timeout = setTimeout(() => {
+        handleNextPage();
+      }, 500); // Delay to prevent rapid calls
+    }
+
+    return () => clearTimeout(timeout);
+  }, [inView, hasNextPage]);
 
   return (
     <div>
       <h1 className={title()}>News</h1>
-      <div className="flex flex-col gap-1 my-4">
-        {data?.pages.map((page, pageIndex) => (
-          <div key={pageIndex}>
-            {page.results.map(
-              (item: News["results"]["data"][number], index: number) => (
-                <Post key={item._id} index={index} post={item} />
-              )
-            )}
-          </div>
-        ))}
-      </div>
+      {data?.pages.map((page, pageIndex) => (
+        <div key={pageIndex}>
+          {page.results.map(
+            (item: News["results"]["data"][number], index: number) => (
+              <div key={item._id} className="flex flex-col gap-1 my-4">
+                <Post index={index} post={item} />
+              </div>
+            )
+          )}
+        </div>
+      ))}
 
       {hasNextPage ? (
         <button onClick={handleNextPage} className="my-4">
@@ -61,6 +78,8 @@ export default function Page() {
 
       {isLoading && <div>Loading...</div>}
       {isError && <div>Error loading data.</div>}
+
+      <div ref={ref} className="h-10" />
     </div>
   );
 }
